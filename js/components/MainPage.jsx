0571c73c4bc04c84fdd3abd15a3810db2f1606de
Silohi Andreea <andreea.silohi@bearingpoint.com>
2023-05-10 17:46:44 +0300

    bug fixed

--- js/components/MainPage.jsx
+++ js/components/MainPage.jsx
@@ -67,7 +67,7 @@ return (
               type="button"
               onClick={deleteRecord}
               id={record._id}
-              className="  mb-5 text-white bg-gradient-to-r from-blue-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 "
+              className="  mb-5 text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 "
             >
               Delete
             </button>
